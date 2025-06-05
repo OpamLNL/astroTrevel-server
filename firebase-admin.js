5f1bbc58bf386ebe483fcff0f4b34281af294909
Abscentee13 <nataliya.linchuk@uzhnu.edu.ua>
2025-06-05 20:44:07 +0300

    GOOGLE_CREDENTIALS test

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./secrets/astrotravel-auth-firebase-adminsdk-fbsvc-a4afaa77e2.json');
+// const serviceAccount = require('./secrets/astrotravel-auth-firebase-adminsdk-fbsvc-a4afaa77e2.json');
 
 //admin.initializeApp({
 //    credential: admin.credential.cert(serviceAccount),
