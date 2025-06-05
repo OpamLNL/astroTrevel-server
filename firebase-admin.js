6dbc21dd6ec8e17e64d3c359e029094c43a51712
OpamLNL <nataliya.linchuk@uzhnu.edu.ua>
2025-06-05 16:36:17 +0300

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,8 +1,12 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./secrets/astrotravel-auth-firebase-adminsdk-fbsvc-a4afaa77e2.json');
 
+//admin.initializeApp({
+//    credential: admin.credential.cert(serviceAccount),
+//});
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_CREDENTIALS)),
 });
 
 module.exports = admin;
